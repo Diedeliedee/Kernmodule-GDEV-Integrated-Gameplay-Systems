b8da1e7da07a79560c09ee73cfa79cae6077419c
Doedeledoedel <jralphen@kpnmail.nl>
2023-10-06 11:31:47 +0200

    Added observer functionality to the inventory

--- GDEV - Crafting Game/Assets/Scripts/Inventory System/Visual/SlotElement.cs
+++ GDEV - Crafting Game/Assets/Scripts/Inventory System/Visual/SlotElement.cs
@@ -4,19 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 
-public class SlotElement : BaseInteractable
+public class SlotElement
 {
-    private Tile connectedTile = null;
+    private readonly Tile connectedTile = null;
 
-    public SlotElement(RectTransform _transform, Tile _connectedTile) : base(_transform)
+    private readonly Image itemImage = null;
+    private readonly TextMeshProUGUI itemText = null;
+
+    public SlotElement(RectTransform _transform, Tile _connectedTile)
     {
         connectedTile = _connectedTile;
+
+        itemImage = _transform.GetChild(0).GetComponent<Image>();
+        itemText = _transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+
+        connectedTile.OnAltered += OnTileAltered;
     }
 
-    public override void OnClick(Vector2 _mousePos)
+    private void OnTileAltered(ItemStack _stack)
     {
-        Debug.Log("HALLOOO!!!");
+        itemImage.sprite = _stack.Item.Image;
+        itemText.text = _stack.Amount.ToString();
     }
 }
